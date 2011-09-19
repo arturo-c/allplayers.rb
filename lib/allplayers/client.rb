565ab29a56b5cd1dbbe95b6bab0ba1d3e13d4063
art kontreras <arturo.kontreras@gmail.com>
2011-09-19 14:05:21 -0500

    SID-18481823: Ruby client switch API's

--- lib/allplayers/client.rb
+++ lib/allplayers/client.rb
@@ -41,7 +41,7 @@ module AllPlayers
 
     def request(verb, path, query = {}, payload = {}, headers = {})
       begin
-        if ['events', 'groups'].include? path
+        if ['albums', 'announcements', 'broadcasts', 'events', 'groups', 'messages', 'photos', 'resources', 'users'].include? path
           uri = Addressable::URI.join(@base_uri, 'api/v1/rest/'+path.to_s)
         else
           uri = Addressable::URI.join(@base_uri, 'api/rest/'+path.to_s)
