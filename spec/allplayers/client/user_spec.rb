efcdbcc5f28401371e273baa90b2f588f388af5b
art kontreras <arturo.kontreras@gmail.com>
2011-09-21 19:26:46 -0500

    SID-18613343: Organize ruby client (more like twitter) and replace TestUnit with RSpec.

--- spec/allplayers/client/user_spec.rb
+++ spec/allplayers/client/user_spec.rb
@@ -1,14 +1,11 @@
 require 'helper'
 
 describe AllPlayers::Client do
-  before do
-    @client = AllPlayers::Client.new
-  end
-  describe ".user_create" do
-    before :each do
+  describe "User Create" do
+    before :all do
       $birthday = Date.new(1983,5,23)
-      $random_first = (0...8).map{65.+(rand(25)).chr}.join if $random_first.nil?
-      $response = $apci_session.user_create(
+      $random_first = (0...8).map{65.+(rand(25)).chr}.join
+      $user = $apci_session.user_create(
         $random_first + '@example.com',
         $random_first,
         'FakeLast',
@@ -16,15 +13,16 @@ describe AllPlayers::Client do
         $birthday
       )
     end
+
     it "should be created properly." do
       # Check user get response.
-      user = $apci_session.user_get($response['uid'])
+      user = $apci_session.user_get($user['uid'])
       # Check mail.
       user['mail'].should == $random_first + '@example.com'
       # Check username.
       user['apci_user_username'].should == $random_first + ' FakeLast'
       # Check profile fields.
-      profile = $apci_session.user_get_profile($response['uid'])
+      profile = $apci_session.user_get_profile($user['uid'])
       # Check name.
       profile['field_firstname']['item'].first['value'].should == $random_first
       profile['field_lastname']['item'].first['value'].should == 'FakeLast'
@@ -32,6 +30,56 @@ describe AllPlayers::Client do
       Date.parse(profile['field_birth_date']['item'].first['value']).to_s.should == $birthday.to_s
       # Check gender (1 = Male, 2 = Female) <= Lame
       profile['field_user_gender']['item'].first['value'].should == "1"
+    end
+
+    it "should be retrievable." do
+      uid = $user['uid']
+      user = $apci_session.user_get(uid)
+      user['uid'].should == uid.to_s
+    end
+
+    it "should not create users with same email (multi-thread checking)." do
+      pending "until we fix duplicate users on prod."
+      for i in 1..4 do
+        name = (0...8).map{65.+(rand(25)).chr}.join
+        user_create_multiple_threads(name)
+      end
+    end
+
+    protected
+
+    def user_create_multiple_threads(name)
+      # Multi-thread
+      threads = []
+      # Set default thread_count to 7, accept global to change it.
+      thread_count = $thread_count.nil? ? 4 : $thread_count
+      succeed = 0
+      failures = 0
+      for i in 1..thread_count do
+        threads << Thread.new {
+          begin
+            birthday = Date.new(1983,5,23)
+            response = $apci_session.user_create(
+              name + '@example.com',
+              name,
+              'FakeLast',
+              'Male',
+              birthday
+            )
+          rescue => e
+            failures += 1
+          else
+            succeed += 1
+          end
+        }
+      end
+      threads.each_index {|i|
+        threads[i].join
+      }
+      succeed.should == 1
+      failures.should == thread_count - 1
+      users = $apci_session.user_list({:mail => name + '@example.com'})
+      users['item'].size.should == 1
     end
   end
 end
--- spec/allplayers_spec.rb
+++ spec/allplayers_spec.rb
@@ -1,9 +1,11 @@
 require 'helper'
 require 'allplayers/api_spec'
 require 'allplayers/start_apci_session'
+require 'allplayers/client/user_spec'
+require 'allplayers/client/node_spec'
 
 describe AllPlayers do
-  describe ".new" do
+  describe "New" do
     it "should return a Allplayers::Client" do
       AllPlayers.new.should be_a AllPlayers::Client
     end
