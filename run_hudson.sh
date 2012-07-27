848c1d6dbf5f8eeeb26698fdb147b99ca76b5921
Arturo Contreras <arturo@allplayers.com>
2012-07-27 13:00:58 -0500

    [#33487963] Add bundler to ruby client, update readme.

--- run_hudson.sh
+++ run_hudson.sh
@@ -1,4 +1,7 @@
 export APCI_REST_TEST_USER=${MERCURY_LOGIN_U}
 export APCI_REST_TEST_PASS=${MERCURY_LOGIN_P}
-rake ci:setup:testunit test CI_REPORTS=results
-rake spec CI_REPORTS=results
+bundle install
+#Run ruby client testunit
+bundle exec rake ci:setup:testunit test CI_REPORTS=results
+#Run ruby client rspec tests
+bundle exec rake spec CI_REPORTS=results
