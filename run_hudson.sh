8c2cb0869fa38f5d8d8131da82a04be0786d2e70
Arturo Contreras <arturo@allplayers.com>
2012-09-05 11:42:51 -0500

    [#35525931] Fix ruby client hudson task by installing gem in hudson dir instead of system.

--- run_hudson.sh
+++ run_hudson.sh
@@ -1,6 +1,6 @@
 export APCI_REST_TEST_USER=${MERCURY_LOGIN_U}
 export APCI_REST_TEST_PASS=${MERCURY_LOGIN_P}
-bundle install
+bundle install --path vendor/bundle
 #Run ruby client testunit
 bundle exec rake ci:setup:testunit test CI_REPORTS=results
 #Run ruby client rspec tests
